7ef46aa48e65669b0fed1c6a48cbb2b81ae6147e
Prith Kami <prithvi40201@gmail.com>
2023-12-25 17:32:58 -0500

    structural change

---
+++ Remix_SimpleStorage/ExtraStorage.sol
@@ -0,0 +1,13 @@
+//SPDX-identifier : MIT 
+pragma solidity ^0.8.0;
+import "./SimpleStorage";
+contract ExtraStorage is SimpleStorage{ // for inheritance use is keyword
+    function store(uint256 _fav_num) public override {
+        //to overrride we need to change function main to virtual and this function should have oveerrride keyword
+        FavNum = _fav_num+5;
+    }
+}
+
+//For inheritance we use the is keyword 
+//To Override a function we have to first change the other function in main contract make it virtual
+//Then change the function in this inherited contract and use the keyword override
